8ede39a064cbb3527b063a16f4d878c1ec14ae6f
Ed <ed.walker@hotmail.co.uk>
2017-01-28 18:49:11 +0000

    jquery added to bandpage

--- js/controller.js
+++ js/controller.js
@@ -1,7 +1,37 @@
 angular.module('RouteControllers', [])
     .controller('HomeController', function($scope) {
-      
+
+    	$(document).ready(function(){
+			// Function to loop through background-images
+			function imageLoop (){
+				$("#rotatorContainer div").last().fadeOut(500,function(){
+					$(this).insertBefore($("#rotatorContainer div").first()).show();
+				});
+			}
+
+			setInterval(function(){
+				imageLoop();
+			}, 5000);
+
+		});
+    	// END OF JQUERY
     })
     .controller('BandController', function($scope){
-    	$scope.title = "Band Page";
+    	
+    	$(document).ready(function(){
+			// BAND PAGE
+			$("p.bandpage-paragraphs").mouseenter(function(){
+				if ($(this).css("position")=="absolute") {
+					$(this).toggleClass("test");
+					$(this).siblings("img").toggleClass("grey-image");
+				}
+			})
+			$("p.bandpage-paragraphs").mouseleave(function(){
+				if ($(this).css("position")=="absolute") {
+					$(this).toggleClass("test");
+					$(this).siblings("img").toggleClass("grey-image");
+				}
+			})
+		});
+
     });
